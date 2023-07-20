d5e32a60d7c3ae8fa7d5c90a28e40accbdb8a619
Sidroded <deinekindaniil21@gmail.com>
2023-07-20 14:08:49 +0200

    Revert "Merge pull request #1 from Sidroded/test"
    This reverts commit 7d444f759fee1876e13cb285403a458c8e248bd8, reversing
    changes made to 9903c898a5b2a25ec5e3998946ef1c3f2ff6cf87.

--- src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        SumCalculator sumCalculator = new SumCalculator();
-        System.out.println(sumCalculator.sum(2));
-    }
-}
